7f1244814eda6afa24c3ebf5dc3f839021ef7d02
bkcarver <bkcarver@gmail.com>
2019-03-31 18:51:00 -0400

    initial commit
    wk 9 assignment

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var llamaFacts = new Vue({
+   el: '#llamaFacts', 
+   data: {
+   fact1: "Llamas are members of the camelid family", 
+   fact2: "Llamas can be 6 feet tall",
+   fact3: "Llamas have an excellent sense of sight, smell and hearing",
+   fact4: "Llamas are very social animals",
+   fact5: "Llamas are known to spit when provoked",
+   fact6: "Llamas are very intelligent, but stubborn",
+   fact7: "Llamas do not have a specific mating season",
+   fact8: "Llamas live between 20 and 30 years",
+   fact9: "Llamas are herbivores", 
+   fact10:true,
+   fact10: "Llamas have inherited fear of coyote, mountain lions and other dog-like animals."
+ }
+});
